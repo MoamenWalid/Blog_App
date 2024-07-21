fe4f7545c46e90a36b83582c4f6af8132e53404d
MoamenWalid <momenelnager@gmail.com>
2024-07-22 02:55:21 +0300

    files which need to commit: [front-end/src/components/posts/PostsLoadMore.jsx], <es3izj5c> branch, 2024/6/22 in 2:55:21

--- front-end/src/components/posts/PostsLoadMore.jsx
+++ front-end/src/components/posts/PostsLoadMore.jsx
@@ -3,30 +3,37 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPostsLoadMore, setCurrentPage, getPostsCount } from '../redux/slices/postSlice';
 
+const PER_PAGE = 6;
+
 const PostsLoadMore = ({ title }) => {
   const dispatch = useDispatch();
   const { postsLoadMore, currentPage, postsCount, loading } = useSelector(state => state.post);  
 
   useEffect(() => {
     dispatch(getPostsCount());
-  }, [])
+    if (currentPage === 1) {
+      dispatch(getPostsLoadMore(currentPage));
+      dispatch(setCurrentPage());
+    }
+  }, []);
 
-  useEffect(() => {
+  const getMore = () => {
     dispatch(getPostsLoadMore(currentPage));
-  }, [dispatch, currentPage])
+    dispatch(setCurrentPage());
+  }
 
   return (
-    <div className='container mx-auto mt-5 px-3'>
+    <div className='container mx-auto mt-5 px-3 sm:px-0'>
       <h1 className="text-[#181A2A] mb-5 font-bold text-[25px]">{ title }</h1>
-      <div className='posts-load-more mb-5  grid gap-4 grid-cols-1 md:grid-cols-3'>
+      <div className='posts-load-more mb-5  grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4'>
         {postsLoadMore.map((post, index) => (
           <PostCart key={`${post?._id}-${index}-post`} post={post} />
         ))}
       </div>
       
-      { currentPage < Math.ceil(postsCount / 6) ?
+      { currentPage <= Math.ceil(postsCount / PER_PAGE) || loading ?
         <div className='flex items-center justify-center'>
-        <button onClick={() => dispatch(setCurrentPage())} className='load-more text-[16px] px-4 py-2 font-medium text-[#696A75] rounded-[6px] border-[#696a75b2] border-solid border-[1px]'>{ loading ? 'Loading...' : 'Load More' }</button>
+        <button onClick={ getMore } className='load-more text-[16px] px-4 py-2 font-medium text-[#696A75] rounded-[6px] border-[#696a75b2] border-solid border-[1px]'>{ loading ? 'Loading...' : 'Load More' }</button>
       </div>  
       : null}
     </div>
