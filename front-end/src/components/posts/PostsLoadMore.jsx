962eb476095ebdfbd0f51403435cbbaa983199bc
MoamenWalid <momenelnager@gmail.com>
2024-07-21 08:31:04 +0300

    files which need to commit: [front-end/src/components/posts/PostsLoadMore.jsx, front-end/src/components/redux/slices/postSlice.js], <35h7bpap> branch, 2024/6/21 in 8:31:3

--- front-end/src/components/posts/PostsLoadMore.jsx
+++ front-end/src/components/posts/PostsLoadMore.jsx
@@ -1,18 +1,34 @@
+import PostCart from './PostCart.jsx';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getPostsLoadMore, setCurrentPage, getPostsCount } from '../redux/slices/postSlice';
 
 const PostsLoadMore = ({ title }) => {
-  
+  const dispatch = useDispatch();
+  const { postsLoadMore, currentPage, postsCount, loading } = useSelector(state => state.post);  
+
+  useEffect(() => {
+    dispatch(getPostsCount());
+  }, [])
+
+  useEffect(() => {
+    dispatch(getPostsLoadMore(currentPage));
+  }, [dispatch, currentPage])
 
   return (
     <div className='container mx-auto mt-5 px-3'>
       <h1 className="text-[#181A2A] mb-5 font-bold text-[25px]">{ title }</h1>
       <div className='posts-load-more mb-5  grid gap-4 grid-cols-1 md:grid-cols-3'>
-        {/* {loadMore.posts.length ? loadMore.posts.map((post, index) => 
-          <PostCart key={`${index}-${post?._id}`} post={post} />
-        ) : null}   */}
+        {postsLoadMore.map((post, index) => (
+          <PostCart key={`${post?._id}-${index}-post`} post={post} />
+        ))}
       </div>
-      <div className='flex items-center justify-center'>
-        <button className='load-more text-[16px] px-4 py-2 font-medium text-[#696A75] rounded-[6px] border-[#696a75b2] border-solid border-[1px]'>Load More</button>
+      
+      { currentPage < Math.ceil(postsCount / 6) ?
+        <div className='flex items-center justify-center'>
+        <button onClick={() => dispatch(setCurrentPage())} className='load-more text-[16px] px-4 py-2 font-medium text-[#696A75] rounded-[6px] border-[#696a75b2] border-solid border-[1px]'>{ loading ? 'Loading...' : 'Load More' }</button>
       </div>  
+      : null}
     </div>
   );
 }
--- front-end/src/components/redux/slices/postSlice.js
+++ front-end/src/components/redux/slices/postSlice.js
@@ -8,30 +8,67 @@ export const getPosts = createAsyncThunk('post/getPosts', async (page, { rejectW
     dispatch(setPosts(data));
 
   } catch (error) {
-    console.log(error);
     toast.error(error.response.data.message);
     return rejectWithValue(error.response.data.message);
   }
 })
 
+export const getPostsLoadMore = createAsyncThunk('post/getPostsLoadMore', async (page, { rejectWithValue }) => {
+  try {
+    const { data } = await req.get(`api/posts?page=${page}`);
+    return data;
+
+  } catch (error) {
+    toast.error(error.response.data.message);
+    return rejectWithValue(error.response.data.message);
+  }
+})
+
+export const getPostsCount = createAsyncThunk('post/getPostsCount', async (page, { rejectWithValue }) => {
+  try {
+    const { data } = await req.get(`api/posts/count`);
+    return data.count;
+
+  } catch (error) {
+    toast.error(error.response.data.message);
+    return rejectWithValue(error.response.data.message);
+  }
+})
+
+
+
 const postSlice = createSlice({
   name: "post",
   initialState: {
     posts: [],
-    loadMore: {posts: [], loading: false},
+    postsLoadMore: [],
+    currentPage: 1,
     postsCount: null,
-    postsCat: []
+    loading: false,
   },
   reducers: {
     setPosts(state, action) {
       state.posts = action.payload
     },
-    setPostsCat(state, action) {
-      state.postsCat = action.payload
+    setCurrentPage(state) {
+      state.currentPage++;
     }
   },
-  extraReducers: (builder) => {}
+  extraReducers: (builder) => {
+    builder.addCase(getPostsLoadMore.pending, (state) => {
+      state.loading = true;
+    })
+
+    builder.addCase(getPostsLoadMore.fulfilled, (state, action) => {
+      state.loading = false;
+      state.postsLoadMore = [...state.postsLoadMore, ...action.payload];
+    })
+
+    builder.addCase(getPostsCount.fulfilled, (state, action) => {
+      state.postsCount = action.payload;
+    })
+  }
 })
 
-export const { setPosts } = postSlice.actions;
+export const { setPosts, setCurrentPage } = postSlice.actions;
 export default postSlice.reducer;
